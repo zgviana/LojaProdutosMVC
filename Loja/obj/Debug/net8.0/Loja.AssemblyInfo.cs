a354887a70e0c30bc9f0d24d7b7ff98a6e099dc8
giovanni.viana <zgviana@gmail.com>
2025-06-30 10:18:25 -0300

    Retirando comentários

--- Loja/obj/Debug/net8.0/Loja.AssemblyInfo.cs
+++ Loja/obj/Debug/net8.0/Loja.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Loja")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab492f8cefc9d6fcd1fc2feb934384401fd1968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5a1872e37f3e68485c3c8867fbca8b2fa9778b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Loja")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Loja")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
